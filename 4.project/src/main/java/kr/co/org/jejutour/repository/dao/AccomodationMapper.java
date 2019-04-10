8da3903020ada085cf214fcdce75a3234051b412
JEONGRAEKIM <kos8469@naver.com>
2019-04-10 13:04:04 +0900

    정래

--- 4.project/src/main/java/kr/co/org/jejutour/repository/dao/AccomodationMapper.java
+++ 4.project/src/main/java/kr/co/org/jejutour/repository/dao/AccomodationMapper.java
@@ -1,10 +1,13 @@
 package kr.co.org.jejutour.repository.dao;
 
+import java.util.List;
+
 import kr.co.org.jejutour.repository.vo.AccInfoVO;
+import kr.co.org.jejutour.repository.vo.CodeVO;
 
 public interface AccomodationMapper {
 
 	public void insertAccInfo(AccInfoVO board);
-	
+	public List<CodeVO> selectCodeTown(int parent_id);
 	
 }
